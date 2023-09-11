6ab1648920fb83fe372fa96321a8b903f38b0c29
Njokimary <marynjoki738@gmail.com>
2023-09-11 22:19:36 +0300

    python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+
+    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+    
 
 def add(num1, num2):
-    pass
+    result =num1 + num2
+    return result
+sum_result = add(45,55)
+print(sum_result)
 
 def halve(number):
-    pass
+    result = number / 2
+    return result
+
+
+integer_input = 10
+halved_integer = halve(integer_input)
+print(halved_integer)
+
+# Call the function with a float input
+float_input = 7.5
+halved_float = halve(float_input)
+print(halved_float)
+
